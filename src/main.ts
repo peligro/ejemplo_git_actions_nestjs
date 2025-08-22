cf7311f486c5f37cebf15cba5eef29b77db3a33a
cesar_cz <yo@cesarcancino.com>
2025-08-22 19:31:16 -0400

    feat: versión exitosa :facepunch:

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
    //habilitar CORS
     app.enableCors();
   await app.listen(`${process.env.CURSO_SERVER_PORT}`);
-  Logger.debug(`Desplegando exitosamnte en el puerto ${process.env.CURSO_SERVER_PORT} desde un Pipeline CI/CD, en AWS con el servicio EC2`)
+  Logger.debug(`Desplegando exitosamnte en el puerto ${process.env.CURSO_SERVER_PORT} desde un Pipeline CI/CD, en AWS con el servicio EC2  `)
 }
 bootstrap();
