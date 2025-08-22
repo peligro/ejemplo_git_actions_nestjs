499a467fac53ac7b5137c01da46cb79aa4b1863b
cesar_cz <yo@cesarcancino.com>
2025-08-22 18:21:40 -0400

    feat: última versión funcionando :facepunch:

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,6 @@ export class AppController {
 
   @Get()
   getHello() {
-    return {"estado": "ok", "mensaje":"Aplicación Nestjs 11 desde gitactions desplegada en EC2 de AWS mediante Pipeline con CI/CD"};
+    return {"estado": "ok", "mensaje":"Aplicación Nestjs 11 desde gitactions desplegada en EC2 de AWS mediante Pipeline con CI/CD, despliegue contínuo"};
   }
 }
